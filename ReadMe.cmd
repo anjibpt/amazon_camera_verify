7fbbe136031c3d22f7202b988bf81bb10038385c
anjibpt <45115455+anjibpt@users.noreply.github.com>
2018-11-18 11:51:23 +0530

    Update ReadMe.cmd

--- ReadMe.cmd
+++ ReadMe.cmd
@@ -1,1 +1,4 @@
-run the project as java application.
+Download amazon_camera_verify project
+under test folder 'Launch_AmazonIndia.java' class file covered the steps as per instructions provided.
+run the 'Launch_AmazonIndia.java'  as java application.
+Note: In instructions mentioned select 8 pieces to cart but it allowed max 3 pieces. Hence selected maximum.
